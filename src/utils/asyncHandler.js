2305f539b7d9e56c962004382f1b284a14f70eef
cyphering101Raman <guptaraman600@gmail.com>
2025-07-27 06:28:48 +0530

    Add Utils - Response, Error, AsyncHandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,11 @@
+const asyncHandler = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      return await requestHandler(req, res, next);
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export { asyncHandler };
