1ed748943f3d75eac1017dcf03adb7d9eb2e2979
cyphering101Raman <guptaraman600@gmail.com>
2025-07-28 22:31:01 +0530

    Updated User Route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,22 @@
 import { Router } from "express";
 import { registerUser, loginUser, logoutUser } from "../controller/user.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
+import User from "../models/user.models.js"
 
 const route = Router()
+
+route.get("/me", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password")
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+})
 
 route.post("/signup", registerUser)
 route.post("/login", loginUser)
